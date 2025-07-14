3fe5fb521440ba03bbd5eab4e09337ca1fb5ebbc
magic62 <trevorlwest222@gmail.com>
2025-07-14 17:19:56 -0400

    Update ItemHeader

--- src/components/ItemHeader.vue
+++ src/components/ItemHeader.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="flex justify-between items-center mb-6">
-        <div class="flex items-center">
-            <img v-if="item" :src="`/icons/${item.icon.toLowerCase()}.png`" :alt="item.name" class="h-12 w-12 mr-4" />
-            <h1 v-if="item" class="text-2xl sm:text-4xl font-bold text-epoch-gold">
-                {{ item.name }}
-            </h1>
+        <div class="flex items-center min-w-0 flex-1 min-h-[3rem]">
+            <div v-if="item" class="flex items-center">
+                <img :src="`/icons/${item.icon.toLowerCase()}.png`" :alt="item.name" class="h-12 w-12 mr-4" />
+                <h1 class="text-2xl sm:text-4xl font-bold text-epoch-gold">
+                    {{ item.name }}
+                </h1>
+            </div>
         </div>
         <button @click="$emit('go-back')"
-            class="bg-epoch-gray-700 hover:bg-epoch-gold text-white font-bold py-2 px-4 rounded transition-colors">
+            class="bg-epoch-gray-700 hover:bg-epoch-gold text-white font-bold py-2 px-4 rounded transition-colors flex-shrink-0">
             Back
         </button>
     </div>
