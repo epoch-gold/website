bb5847bce3a65c303d390e4a35bed65d10330910
magic62 <trevorlwest222@gmail.com>
2025-07-14 19:06:01 -0400

    Add buttons

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <div class="flex flex-col w-full mx-auto min-h-screen bg-epoch-gray-900">
-    <Header />
+    <div class="w-full bg-epoch-gray-900">
+      <Header />
+    </div>
     <main class="flex-grow w-full max-w-7xl mx-auto">
       <router-view />
     </main>
+    <div class="w-full bg-epoch-gray-900">
+      <Footer />
+    </div>
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     Header,
+    Footer,
   },
 });
 </script>
